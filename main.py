b029d2dc22d03f3949ef8e22769f9d04bed08081
YusupovRuslan <yusupov.ruslan.sh@gmail.com>
2024-05-30 14:26:13 +0500

    Change path

--- main.py
+++ main.py
@@ -22,9 +22,9 @@ file_id = '1aU0M0sBQ55vhKxXJaaD8B9Bu_O60s5zG'
 url = f'https://drive.google.com/uc?id={file_id}'
 
 # Загрузка файла
-gdown.download(url, 'C:\\Users\\yusup\\OneDrive\\Рабочий стол\\sales_stock\\Data\\raw\\PE.xlsx', quiet=False)
+gdown.download(url, 'D:\\project\\sales_stock\\Data\\raw', quiet=False)
 
-data_prep = DataPreparation('C:\\Users\\yusup\\OneDrive\\Рабочий стол\\sales_stock\\Data\\raw\\PE.xlsx')
+data_prep = DataPreparation('C:\\project\\sales_stock\\Data\\raw\\PE.xlsx')
 grouped_sales = data_prep.load_data()
 df_pr = data_prep.remove_anomalies(grouped_sales)
 df_pr['ds'] = pd.to_datetime(df_pr['ds']) # Преобразование в формат даты уже явно задал
@@ -42,26 +42,13 @@ graph_model = model.plot(forecast, figsize=(10, 6))
 plt.show(block=True)
 
 # Создание таблицы и сохранение ее в CSV-файл
-table = create_table(forecast, df_pr, 'C:\\Users\\yusup\\OneDrive\\Рабочий стол\\sales_stock\\Data\\Proccesed\\table.csv')
+table = create_table(forecast, df_pr, 'D:\\project\\sales_stock\\Data\\Proccesed\\table.csv')
 
 # Визуализация таблицы
 graph_forecast = plot_table(table)
 print(type(graph_forecast))
 
-# экспорт файла на гугл диск
-# Аутентификация и создание объекта GoogleDrive
-#gauth = GoogleAuth()
-#gauth.LoadCredentialsFile('C:\\Users\\yusup\\Downloads\\')
-#drive = GoogleDrive(gauth)
 
-# Создание объекта GoogleDriveFile
-#file = drive.CreateFile({'title': 'table.csv',
-                         'parents': [{'id': }]})
-
-# Загрузка файла на Google Drive
-#file.SetContentFile('C:\\Users\\yusup\\OneDrive\\Рабочий стол\\sales_stock\\Data\\Proccesed\\table.csv')
-#file.Upload()
-#print("Файл успешно загружен на Google Диск!")
 
 
 #%%
